b2e944b6390e234ed39d4f1af1c456d2464aa30f
Tarik <tariksalhiars@gmail.com>
2023-05-19 11:44:24 +0100

    adding mon

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = 5000;
@@ -11,7 +12,28 @@ app.use(express.json());
 
 /** Connect to MongoDB */
 
-const uri = "mongodb://localhost:27017/mern-todo-app";
+const uri =
+  "mongodb+srv://tariksalhiars:aufdVQpNTe4rqFeZ@cluster0.frraiph.mongodb.net/?retryWrites=true&w=majority";
+
+  // Create a new MongoClient
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
 mongoose.connect(uri);
 
 const connection = mongoose.connection;
